5522304f212ebdb26adccf118c805dcb7ea77b33
virdausa <team.cobusi@gmail.com>
2025-06-19 10:15:43 +0700

    fix: dump bbrp file blades yang ngga kepake :( backup, cek lagi nanti, bodo nerpai v0

--- resources/views/company/suppliers/partials/actions.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="flex gap-3 justify-end">
-    <x-button-show :route="route('suppliers.show', $supplier->id)" />
-    <!-- <x-button-edit :route="route('suppliers.edit', $supplier->id)" /> -->
-    <x-button-delete :route="route('suppliers.destroy', $supplier->id)" />
-</div>
